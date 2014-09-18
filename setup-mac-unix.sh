c9ab05b75b3f0753d169f98f95b2174368343574
Daniel Hough <daniel.hough@gmail.com>
2014-09-18 13:30:41 +0100

    Create setup-mac-unix.sh

---
+++ setup-mac-unix.sh
@@ -0,0 +1,6 @@
+alias jerk='git'
+git config --global alias.it pull
+git config --global alias.off commit
+git config --global alias.bank stash
+git config --global alias.to push
+git config --global alisa.with merge
