bfbc960ccc3176df59ccf63668daa6092cdf3fd5
jojoe77777 <j@melban.com>
2016-01-28 17:32:03 +1100

    Sorry, missed that one

--- windows.bat
+++ windows.bat
@@ -3,4 +3,4 @@ git config --global alias.it pull
 git config --global alias.off commit
 git config --global alias.bank stash
 git config --global alias.to push
-git config --global alisa.with merge
+git config --global alias.with merge
